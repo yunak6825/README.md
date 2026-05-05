32c2a9df71d2ce50159eea7457c784c4926fcc2e
yunak6825 <yunak6825@gmail.com>
2026-05-05 16:27:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # 1. AI 설정 (AI Studio에서 만든 설정 그대로)
-genai.configure(api_key="AIzaSyDFjkNKawokylB-XHwDUYFM1vi-4otR288")
+genai.configure(api_key="AIzaSyDltq-V982BHHN2goae9kPTs2yg-qYzKjs")
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # 2. 웹 화면 꾸미기
